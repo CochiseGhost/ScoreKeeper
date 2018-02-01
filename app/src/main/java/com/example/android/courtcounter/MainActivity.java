19525b87ea9f946da48681ee85eb4c42d9415f1b
CochiseGhost <35630992+CochiseGhost@users.noreply.github.com>
2018-02-01 14:22:13 -0600

    added strings in place of hard code,added scrollview,added java onSaveInstanceState and onRestoreInstanceState

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -9,64 +9,105 @@ public class MainActivity extends AppCompatActivity {
     int scoreTeamA = 0;
     int scoreTeamB = 0;
 
+    private TextView scoreForTeamA;
+    private TextView scoreForTeamB;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(0);
+        // Initializes all text views in one call
+        scoreForTeamA = findViewById(R.id.team_a_score);
+        scoreForTeamB = findViewById(R.id.team_b_score);
+
+    }
+
+
+    /**
+     * Saves app data between states
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("scoreTeamA", scoreTeamA);
+        outState.putInt("scoreTeamB", scoreTeamB);
+
+    }
+
+    /**
+     * Restores app data on new state
+     */
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        scoreTeamA = savedInstanceState.getInt("scoreTeamA");
+        scoreTeamB = savedInstanceState.getInt("scoreTeamB");
+
     }
 
     /**
      * Displays the given score for Team A.
      */
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        TextView scoreView = (scoreForTeamA);
         scoreView.setText(String.valueOf(score));
     }
+
     /**
      * Displays the given score for Team B.
      */
     public void displayForTeamB(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        TextView scoreView = (scoreForTeamB);
         scoreView.setText(String.valueOf(score));
     }
-    public void addSixForTeamB (View V) {
-     scoreTeamB = scoreTeamB + 6;
-     displayForTeamB(scoreTeamB);
+
+    public void addSixForTeamB(View V) {
+        scoreTeamB = scoreTeamB + 6;
+        displayForTeamB(scoreTeamB);
     }
-    public void addOneForTeamB (View V) {
+
+    public void addOneForTeamB(View V) {
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
     }
-    public void addThreeForTeamB (View V) {
+
+    public void addThreeForTeamB(View V) {
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
     }
+
     public void addTwoForTeamB(View V) {
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
     }
 
-public void addSixForTeamA(View V) {
+    public void addSixForTeamA(View V) {
         scoreTeamA = scoreTeamA + 6;
         displayForTeamA(scoreTeamA);
-}
+    }
+
     public void addOneForTeamA(View V) {
         scoreTeamA = scoreTeamA + 1;
-    displayForTeamA(scoreTeamA);
+        displayForTeamA(scoreTeamA);
     }
+
     public void addThreeForTeamA(View V) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
+
     public void addTwoForTeamA(View V) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
-    public void resetScore (View V) {
+
+    public void resetScore(View V) {
         scoreTeamA = 0;
         scoreTeamB = 0;
-    displayForTeamA(scoreTeamA);
-    displayForTeamB(scoreTeamB);
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
     }
+
+
 }
